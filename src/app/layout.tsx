c70b24d422abc1a316b5aaadebe22fde0d5ca0d5
vishal <jakharvishalv@gmail.com>
2024-05-28 23:32:02 +0530

    zap zap zap

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import { Toaster } from "react-hot-toast";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Trim-Shorten Your Links",
+    description: "Shorten your links with ease using Trim",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${inter.className} relative  bg-black text-white min-h-screen max-w-[1200px] m-auto`}>
+                <Toaster />
+                <Navbar />
+                {children}
+            </body>
+        </html>
+    );
+}
